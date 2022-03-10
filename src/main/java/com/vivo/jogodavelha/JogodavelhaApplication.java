4178053a95f43a06f7bc83b70c9f4f6d0c839bc4
Ian Heski Maia <40414162@redecorp.br>
2022-03-10 11:19:55 -0300

    Initial commit, basic spring structure

---
+++ src/main/java/com/vivo/jogodavelha/JogodavelhaApplication.java
@@ -0,0 +1,13 @@
+package com.vivo.jogodavelha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JogodavelhaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JogodavelhaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vivo/jogodavelha/JogodavelhaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vivo.jogodavelha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JogodavelhaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
